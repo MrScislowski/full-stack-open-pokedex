b44bca1e490aa991dd3f5a2f1aa49d62df50e65b
Daniel Scislowski <mr.scislowski@gmail.com>
2024-09-26 11:55:20 -0500

    commit #2

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 app.get("/version", (req, res) => {
+  // unnecessary comment...
   res.send(
     "This merge is good... has no #skips"
   );
