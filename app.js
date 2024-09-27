04e23a741447e7229d16f49f68a192cd6aa916da
Daniel Scislowski <mr.scislowski@gmail.com>
2024-09-27 07:52:44 -0500

    fix the build, should now work

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 app.get("/version", (req, res) => {
-  console.log("deliberate syntax error:"))
   res.send(
     "I don't even know any more... check on github"
   );
-
 });
 
 app.get("/health", (req, res) => {
